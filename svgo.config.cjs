87bab15a5a798ee2b9350344341b00098b747426
vadim <vadim1884321@gmail.com>
2023-12-01 14:33:57 +0300

    📝 chore(svgo.config.cjs): добавлен файл конфигурации для SVGO
    Добавлен файл svgo.config.cjs, содержащий конфигурацию для SVGO. В конфигурации установлены следующие параметры:
    - multipass: true
    - js2svg.indent: 2
    - js2svg.pretty: true, если NODE_ENV не равно 'production', иначе false
    - plugins: добавлены два плагина - preset-default и sortAttrs. Плагин preset-default имеет параметр overrides, в котором установлено значение removeViewBox: false. Плагин sortAttrs имеет параметр xmlnsOrder, в котором установлено значение 'alphabetical'.

---
+++ svgo.config.cjs
@@ -0,0 +1,25 @@
+// https://github.com/svg/svgo#configuration
+const isProduction = process.env.NODE_ENV === 'production';
+module.exports = {
+	multipass: true,
+	js2svg: {
+		indent: 2,
+		pretty: !isProduction,
+	},
+	plugins: [
+		{
+			name: 'preset-default',
+			params: {
+				overrides: {
+					removeViewBox: false,
+				},
+			},
+		},
+		{
+			name: 'sortAttrs',
+			params: {
+				xmlnsOrder: 'alphabetical',
+			},
+		},
+	],
+};
