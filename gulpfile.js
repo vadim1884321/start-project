e000634e303518808c90844ba23e25a1e72ce3be
vadim <vadim1884321@gmail.com>
2023-12-01 14:31:07 +0300

    🔧 fix(gulpfile.js): закомментированы неиспользуемые импорты и переменные ✨ feat(gulpfile.js): заменены require на import и использованы модули ES6 🔧 fix(gulpfile.js): изменены пути к файлам и папкам для работы с Gulp ✨ feat(gulpfile.js): добавлены экспорты для задач nunjucks, styles, scripts, svg, serve и watch 🔧 fix(gulpfile.js): изменены вызовы задач на использование Gulp API ✨ feat(gulpfile.js): добавлены задачи build и default для сборки проекта и запуска сервера

--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,161 @@
-const fs = require('fs');
-const rimraf = require('rimraf');
-const { src, dest, series, parallel, watch } = require('gulp');
-const nunjucksRender = require('gulp-nunjucks-render');
-const posthtml = require('gulp-posthtml');
-// const htmlBeautify = require('gulp-html-beautify');
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const esbuild = require('gulp-esbuild');
-const rename = require('gulp-rename');
-const replace = require('gulp-replace');
-const plumber = require('gulp-plumber');
-const svgmin = require('gulp-svgmin');
-const svgstore = require('gulp-svgstore');
-const browserSync = require('browser-sync');
+// const fs = require('fs');
+// const rimraf = require('rimraf');
+// const { src, dest, series, parallel, watch } = require('gulp');
+// const nunjucksRender = require('gulp-nunjucks-render');
+// const posthtml = require('gulp-posthtml');
+// // const htmlBeautify = require('gulp-html-beautify');
+// const sass = require('gulp-sass')(require('sass'));
+// const postcss = require('gulp-postcss');
+// const esbuild = require('gulp-esbuild');
+// const rename = require('gulp-rename');
+// const replace = require('gulp-replace');
+// const plumber = require('gulp-plumber');
+// const svgmin = require('gulp-svgmin');
+// const svgstore = require('gulp-svgstore');
+// const browserSync = require('browser-sync');
+import * as fs from 'node:fs';
+import gulp from 'gulp';
+import { rimraf } from 'rimraf';
+import nunjucksRender from 'gulp-nunjucks-render';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import lightningcss from 'gulp-lightningcss';
+import esbuild from 'gulp-esbuild';
+import rename from 'gulp-rename';
+import replace from 'gulp-replace';
+import plumber from 'gulp-plumber';
+import svgmin from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import browserSync from 'browser-sync';
+
+const sass = gulpSass(dartSass);
 
 const isProduction = process.env.NODE_ENV == 'production';
 
 // Разметка
-const nunjucks = () => {
-  return (
-    src('src/_includes/pages/**/*.{html,njk}')
-      .pipe(
-        plumber({
-          errorHandler: function (err) {
-            console.log(err.message);
-            this.emit('end');
-          },
-        })
-      )
-      .pipe(
-        nunjucksRender({
-          path: ['src/_includes/'],
-          envOptions: {
-            trimBlocks: true,
-            lstripBlocks: true,
-          },
-          data: {
-            nav: JSON.parse(fs.readFileSync('src/_data/navigation.json', 'utf8')),
-            site: JSON.parse(fs.readFileSync('src/_data/site.json', 'utf8')),
-            year: new Date().getFullYear(),
-          },
-        })
-      )
-      // .pipe(htmlBeautify())
-      .pipe(posthtml())
-      .pipe(replace(/\.\.\//g, './'))
-      .pipe(dest('./src/'))
-      .pipe(browserSync.stream())
-  );
+export const nunjucks = () => {
+	return (
+		gulp
+			.src('./src/_includes/pages/**/*.{html,njk}')
+			.pipe(
+				plumber({
+					errorHandler: function (err) {
+						console.log(err.message);
+						this.emit('end');
+					},
+				})
+			)
+			.pipe(
+				nunjucksRender({
+					path: ['./src/_includes'],
+					envOptions: {
+						trimBlocks: true,
+						lstripBlocks: true,
+					},
+					data: {
+						nav: JSON.parse(fs.readFileSync('./src/_data/navigation.json', 'utf8')),
+						site: JSON.parse(fs.readFileSync('./src/_data/site.json', 'utf8')),
+						year: new Date().getFullYear(),
+					},
+				})
+			)
+			// .pipe(htmlBeautify())
+			// .pipe(posthtml())
+			.pipe(replace(/\.\.\//g, './'))
+			.pipe(gulp.dest('./src'))
+			.pipe(browserSync.stream())
+	);
 };
 
 // Обработка стилей
-const styles = () => {
-  return src('src/scss/**/*.scss', { sourcemaps: !isProduction })
-    .pipe(
-      plumber({
-        errorHandler: function (err) {
-          console.log(err.message);
-          this.emit('end');
-        },
-      })
-    )
-    .pipe(sass({ outputStyle: 'expanded' }))
-    .pipe(postcss())
-    .pipe(replace(/(\.\.\/|\.\/){1,}/g, '../'))
-    .pipe(dest('./src/css/', { sourcemaps: !isProduction }))
-    .pipe(browserSync.stream());
+export const styles = () => {
+	return gulp
+		.src('src/scss/**/*.scss', { sourcemaps: !isProduction })
+		.pipe(
+			plumber({
+				errorHandler: function (err) {
+					console.log(err.message);
+					this.emit('end');
+				},
+			})
+		)
+		.pipe(sass({ outputStyle: 'expanded' }))
+		.pipe(lightningcss({ minify: isProduction }))
+		.pipe(replace(/(\.\.\/|\.\/){1,}/g, '../'))
+		.pipe(gulp.dest('./src/css/', { sourcemaps: !isProduction }))
+		.pipe(browserSync.stream());
 };
 
 // Обработка скриптов
-const scripts = () => {
-  return src('./src/js/main.js', { sourcemaps: !isProduction })
-    .pipe(
-      plumber({
-        errorHandler: function (err) {
-          console.log(err.message);
-          this.emit('end');
-        },
-      })
-    )
-    .pipe(
-      esbuild({
-        target: 'es6',
-        outfile: 'main.bundle.js',
-        bundle: true,
-        minify: isProduction,
-        sourcemap: !isProduction,
-        // external: ['jquery'],
-      })
-    )
-    .pipe(dest('./src/js/', { sourcemaps: !isProduction }))
-    .pipe(browserSync.stream());
+export const scripts = () => {
+	return gulp
+		.src('./src/js/main.js', { sourcemaps: !isProduction })
+		.pipe(
+			plumber({
+				errorHandler: function (err) {
+					console.log(err.message);
+					this.emit('end');
+				},
+			})
+		)
+		.pipe(
+			esbuild({
+				target: 'es6',
+				outfile: 'main.bundle.js',
+				bundle: true,
+				minify: isProduction,
+				sourcemap: !isProduction,
+				// external: ['jquery'],
+			})
+		)
+		.pipe(gulp.dest('./src/js', { sourcemaps: !isProduction }))
+		.pipe(browserSync.stream());
 };
 
 // Очистка папки c svg-спрайтом
-const svgClean = () => {
-  return rimraf('./src/images/svg-sprite/**/*');
+export const svgClean = () => {
+	return rimraf('./src/images/svg-sprite/**/*');
 };
 
 // Обработка SVG, создание svg-спрайта
-const svgSprite = () => {
-  return src('./src/images/svg-icons/**/*.svg')
-    .pipe(svgmin())
-    .pipe(svgstore({ inlineSvg: true }))
-    .pipe(rename('sprite.svg'))
-    .pipe(dest('./src/images/svg-sprite/'));
+export const svgSprite = () => {
+	return gulp
+		.src('./src/images/svg-icons/**/*.svg')
+		.pipe(svgmin())
+		.pipe(svgstore({ inlineSvg: true }))
+		.pipe(rename('sprite.svg'))
+		.pipe(gulp.dest('./src/images/svg-sprite'));
 };
 
-const svg = series(svgClean, parallel(svgSprite));
+export const svg = gulp.series(svgClean, gulp.parallel(svgSprite));
 
 // Автоперезагрузка браузера (Live Server)
-const serve = () => {
-  browserSync.init({
-    server: {
-      baseDir: './src',
-    },
-    ui: false,
-    notify: false,
-    open: true,
-    // tunnel: true,
-    // tunnel: 'projectname', //Demonstration page: http://projectname.localtunnel.me
-  });
-  // browserSync.watch('./src/*.html', browserSync.reload);
+export const serve = () => {
+	browserSync.init({
+		server: {
+			baseDir: './src',
+		},
+		ui: false,
+		notify: false,
+		// tunnel: true,
+		// tunnel: 'projectname', //Demonstration page: http://projectname.localtunnel.me
+	});
+	// browserSync.watch('./src/*.html', browserSync.reload);
 };
 
-const watcher = () => {
-  watch('./src/_includes/**/*.{html,njk}', series(nunjucks));
-  watch('./src/scss/**/*.scss', series(styles));
-  watch('./src/js/**/!(main.bundle.js)*.js', series(scripts));
-  watch('./src/images/svg-icons/**/*.svg', series(svg));
+export const watch = () => {
+	gulp.watch('./src/_includes/**/*.{html,njk}', gulp.series(nunjucks));
+	gulp.watch('./src/scss/**/*.scss', gulp.series(styles));
+	gulp.watch('./src/js/**/!(main.bundle.js)*.js', gulp.series(scripts));
+	gulp.watch('./src/images/svg-icons/**/*.svg', gulp.series(svg));
 };
 
-exports.svg = svg;
-exports.nunjucks = nunjucks;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.serve = serve;
-exports.watcher = watcher;
+// exports.svg = svg;
+// exports.nunjucks = nunjucks;
+// exports.styles = styles;
+// exports.scripts = scripts;
+// exports.serve = serve;
+// exports.watch = watch;
 
-exports.build = series(parallel(nunjucks, styles, scripts, svg));
-exports.default = series(parallel(nunjucks, styles, scripts, svg), parallel(watcher, serve));
+export const build = gulp.series(gulp.parallel(nunjucks, styles, scripts, svg));
+export default gulp.series(gulp.parallel(nunjucks, styles, scripts, svg), gulp.parallel(watch, serve));
