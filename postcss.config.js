869b41e33a912d53ea4cb2a87c53117cc9254889
vadim <vadim1884321@gmail.com>
2023-12-01 14:30:21 +0300

    🔥 refactor(gulpfile copy.js): удаление неиспользуемых зависимостей и задач из gulpfile copy.js 🔥 refactor(index.js): удаление неиспользуемого кода из index.js 🔥 refactor(postcss.config.js): удаление неиспользуемого файла postcss.config.js 🔥 refactor(ht.access): удаление неиспользуемого файла ht.access 🔥 refactor(svgo.config.js): удаление неиспользуемого файла svgo.config.js

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = (ctx) => ({
-  plugins: [
-    require('autoprefixer')(),
-    require('postcss-preset-env')({ stage: 0 }),
-    require('postcss-sort-media-queries')(),
-    ctx.env === 'production' ? require('cssnano')() : false,
-  ],
-});
