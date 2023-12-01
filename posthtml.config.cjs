9948d6a409060e4d88001d2d24c4cc2f6bb38ade
vadim <vadim1884321@gmail.com>
2023-12-01 14:32:16 +0300

    🔧 chore(posthtml.config): переименован файл posthtml.config.js в posthtml.config.cjs 🔥 refactor(posthtml.config): удален файл posthtml.config.js

---
+++ posthtml.config.cjs
@@ -0,0 +1,13 @@
+module.exports = (ctx) => ({
+	plugins: [
+		ctx.env !== 'production'
+			? require('posthtml-beautify')({
+					rules: {
+						indent: 2,
+						blankLines: false,
+						sortAttr: true,
+					},
+			  })
+			: false,
+	],
+});
