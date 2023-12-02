8b50682cbcc0eb3adf0a9897b24d2972bc7f59d7
vadim <vadim1884321@gmail.com>
2023-12-02 22:52:08 +0300

    🔧 chore(stylelint.config.mjs): обновлены правила stylelint
    - Добавлено правило 'no-descending-specificity' для отключения проверки на убывающую специфичность
    - Изменено значение правила 'color-function-notation' на 'legacy'
    - Изменено значение правила 'hue-degree-notation' на 'number'
    - Изменено значение правила 'alpha-value-notation' на 'number'
    - Добавлено правило 'selector-class-pattern' для проверки паттерна классов селекторов
    - Добавлены опции для правила 'selector-class-pattern': severity: 'warning', resolveNestedSelectors: true

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -4,6 +4,17 @@ export default {
 	plugins: ['stylelint-scss'],
 	rules: {
 		'at-rule-no-unknown': null,
+		'no-descending-specificity': null,
+		'color-function-notation': 'legacy',
+		'hue-degree-notation': 'number',
+		'alpha-value-notation': 'number',
+		'selector-class-pattern': [
+			'^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
+			{
+				severity: 'warning',
+				resolveNestedSelectors: true,
+			},
+		],
 	},
 	ignoreFiles: ['**/node_modules/**'],
 };
